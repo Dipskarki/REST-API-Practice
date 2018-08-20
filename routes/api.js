8927813f889450aa95bc57cc7116b4bef4a70dbe
Dips <Dipskarki2014@gmail.com>
2018-08-20 17:53:16 +0545

    geoNear implemented

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,26 @@ const Ninja = require('../models/ninja');
 
 //get the list of ninjas from the db
 router.get('/ninjas', function(req, res, next){
-  res.send({type: "GET"});
+  //res.send({type: "GET"});
+  /*Ninja.find({}).then(function(ninjas){
+    res.send(ninjas);
+  })*/
+  // Ninja.geoNear(
+  //   {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+  //   {maxDistance: 100000, spherical: true}
+  // )
+  Ninja.aggregate([
+      {
+        $geoNear:{
+          near:{ type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+          distanceField: 'distance',
+          maxDistance: 100000, 
+          spherical: true
+        }
+      }
+    ]).then(function(ninjas){
+    res.send(ninjas);
+  }).catch(next);
 });
 
 //add a new ninja to the db
